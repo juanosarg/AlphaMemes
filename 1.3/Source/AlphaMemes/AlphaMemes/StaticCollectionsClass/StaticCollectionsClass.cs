6b9cf9239cb47e5c0d8405d6d1e1c63f0b41c67d
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-10-07 22:46:39 +0200

    v0.1

--- 1.3/Source/AlphaMemes/AlphaMemes/StaticCollectionsClass/StaticCollectionsClass.cs
+++ 1.3/Source/AlphaMemes/AlphaMemes/StaticCollectionsClass/StaticCollectionsClass.cs
@@ -20,9 +20,17 @@ namespace AlphaMemes
 
         public static float artBeautyInTheMap = 0;
 
-        
+        public static Dictionary<Pawn, int> colonist_and_random_mood = new Dictionary<Pawn, int>();
 
-     
+
+        public static void AddColonistRandomMood(Pawn pawn, int mood)
+        {
+            if (!colonist_and_random_mood.ContainsKey(pawn))
+            {
+                colonist_and_random_mood.Add(pawn, mood);
+            }
+            else { colonist_and_random_mood[pawn] = mood; }
+        }
 
     }
 }
