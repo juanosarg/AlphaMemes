3b8c4f03b40e77833d84c540acdc63300a2cec87
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-10-10 15:03:42 +0200

    v0.1

--- 1.3/Source/AlphaMemes/AlphaMemes/Precepts/PreceptWorker_DespisedAnimal.cs
+++ 1.3/Source/AlphaMemes/AlphaMemes/Precepts/PreceptWorker_DespisedAnimal.cs
@@ -15,7 +15,7 @@ namespace AlphaMemes {
 		{
 			get
 			{
-				foreach (ThingDef item in DefDatabase<ThingDef>.AllDefs.Where((ThingDef x) => x.thingCategories != null && x.thingCategories.Contains(ThingCategoryDefOf.Animals) && !x.race.Insect && !x.race.Dryad))
+				foreach (ThingDef item in DefDatabase<ThingDef>.AllDefs.Where((ThingDef x) => x.thingCategories != null && x.thingCategories.Contains(ThingCategoryDefOf.Animals) && !x.race.Dryad))
 				{
 					PreceptThingChance preceptThingChance = default(PreceptThingChance);
 					preceptThingChance.def = item;
