67fbc65617a3d8372eed486fff4ebe768a81d03d
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-12-09 13:25:38 +0100

    v1.1

---
+++ 1.3/Source/AlphaMemes/AlphaMemes/Abilities/CompAbilityChangeStyle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld.Planet;
+using RimWorld;
+using Verse;
+using System.Linq;
+
+namespace AlphaMemes
+{
+    public class CompAbilityChangeStyle : CompAbilityEffect
+    {
+
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+
+            base.Apply(target, dest);
+
+            List<ThingStyleCategoryWithPriority> listStyles = Current.Game.World.factionManager.OfPlayer.ideos.PrimaryIdeo.thingStyleCategories;
+
+           
+
+            HashSet<Thing> hashSet = new HashSet<Thing>(target.Cell.GetThingList(this.parent.pawn.Map));
+            if (hashSet != null)
+            {
+                foreach (Thing thing in hashSet)
+                {
+                    if (thing != null && thing.def.CanBeStyled())
+                    {
+
+
+                        thing.StyleDef = listStyles.RandomElement().category.GetStyleForThingDef(thing.def);
+                        thing.DirtyMapMesh(thing.Map);
+                    }
+
+                }
+            }
+
+
+
+
+
+
+
+
+                }
+
+
+
+    }
+}
---
+++ 1.3/Source/AlphaMemes/AlphaMemes/Abilities/Properties/CompProperties_AbilityChangeStyle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Verse;
+using RimWorld;
+
+namespace AlphaMemes
+{
+	public class CompProperties_AbilityChangeStyle : CompProperties_AbilityEffect
+	{
+		public CompProperties_AbilityChangeStyle()
+		{
+			this.compClass = typeof(CompAbilityChangeStyle);
+		}
+
+
+	}
+}
