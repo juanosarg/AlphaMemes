7d1886e798b9d85bb4dfc036921ed2833467d77c
ANRimworld <allie.noble.an@gmail.com>
2022-07-28 15:54:28 -0700

    Always someone who tries to break it.

---
+++ 1.3/Source/AlphaMemes/AlphaMemes/Rituals/Tantrum/RitualBehaviorWorker_Tantrum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace AlphaMemes
+{
+    public class RitualBehaviorWorker_Tantrum : RitualBehaviorWorker
+    {
+        //Users and there thinking up ways to break your memes
+        public RitualBehaviorWorker_Tantrum()
+        {
+        }
+        public RitualBehaviorWorker_Tantrum(RitualBehaviorDef def) : base(def)
+        {
+        }
+        public override string CanStartRitualNow(TargetInfo target, Precept_Ritual ritual, Pawn selectedPawn = null, Dictionary<string, Pawn> forcedForRole = null)
+        {
+            if (!target.Map.IsPlayerHome)
+            {
+                return "AM_TantrumMustBePlayerHome".Translate();
+            }
+            return base.CanStartRitualNow(target, ritual, selectedPawn, forcedForRole);
+        }
+    }
+}
