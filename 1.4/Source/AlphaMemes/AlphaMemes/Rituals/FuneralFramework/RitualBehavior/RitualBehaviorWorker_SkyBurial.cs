eddbe8b80aeede1e1feb2a05fb2214c64b9727eb
ANRimworld <allie.noble.an@gmail.com>
2023-12-15 08:14:19 -0800

    Sky Burial Eject corpse if the Ritual failed

---
+++ 1.4/Source/AlphaMemes/AlphaMemes/Rituals/FuneralFramework/RitualBehavior/RitualBehaviorWorker_SkyBurial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.Sound;
+using Verse.AI;
+
+using Verse.AI.Group;
+using RimWorld;
+
+
+namespace AlphaMemes
+{
+    public class RitualBehaviorWorker_SkyBurial : RitualBehaviorWorker_FuneralFramework 
+    {
+
+        public RitualBehaviorWorker_SkyBurial()
+        {
+        }
+        public RitualBehaviorWorker_SkyBurial(RitualBehaviorDef def) : base(def)
+        {
+        }
+
+        public override void Cleanup(LordJob_Ritual ritual)
+        {
+            //If the funeral failed throw the body out so it can be completed properly, as can't be manually removed
+            if (ritual.Ritual.activeObligations.Contains(ritual.obligation))
+            {
+                var skyBurialSpot = ritual.selectedTarget.Thing as Building_SkyBurialSpot;
+                if (skyBurialSpot != null)
+                {
+                    skyBurialSpot.EjectContents();
+                }                
+            }
+            base.Cleanup(ritual);
+        }
+
+    }
+}
